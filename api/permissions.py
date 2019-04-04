4f9800ba553510990e5551d330b1c32f62adc874
Jangyeon Jin <jyjin@sentinelprotocol.io>
2019-04-04 18:05:10 +0900

    set permission for caselist, case detail

--- api/permissions.py
+++ api/permissions.py
@@ -1,5 +1,8 @@
 from rest_framework import permissions
 from .settings import api_settings
+from .models import (
+    UserPermission, CaseStatus
+)
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
@@ -33,4 +36,39 @@ class InternalOnly(permissions.BasePermission):
 
         if ip.startswith("127") or ip.startswith("10.") or ip.startswith("172.") or ip.startswith("192.") or ip.startswith("255."):
             return True
+        return False
+
+
+class CheckCaseDetailPermission(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if not request and not request.user:
+            return False
+
+        if request.user.permission in [UserPermission.SENTINEL, UserPermission.SUPERSENTINEL]:
+            return True
+        else:
+            if obj.reporter == request.user:
+                return True
+            elif request.user.permission is UserPermission.EXCHANGE and obj.status in [CaseStatus.CONFIRMED, CaseStatus.RELEASED]:
+                return True
+            else:
+                return False
+
+
+class CaseListPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if not request and not request.user:
+            return False
+
+        if request.user.permission in [UserPermission.SENTINEL, UserPermission.SUPERSENTINEL]:
+            return True
+
+        full_path = request.get_full_path()
+        full_path = full_path.split('&')[0]
+
+        if full_path in ['/case?case=my', '/case?case=my_new', '/case?case=my_progress', '/case?case=my_confirmed', '/case?case=my_rejected', '/case?case=my_released']:
+            return True
+        if request.user.permission is UserPermission.EXCHANGE and full_path in ['/case?case=all', '/case?case=all_confirmed', '/case?case=all_released']:
+            return True
+
         return False
