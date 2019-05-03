9168ba0d08d75171362517d19b889de5e26f7a7f
Jangyeon Jin <jyjin@sentinelprotocol.io>
2019-05-03 15:32:52 +0900

    save indicators to local cache

--- api/apps.py
+++ api/apps.py
@@ -1,46 +1,49 @@
-from django.apps import AppConfig
+from django.apps import AppConfig, apps
 from celery import Celery
+from django.conf import settings
+from .cache import DefaultCache
 from django.conf import settings
 import requests
 import json
 import os
-from .cache import DefaultCache
 
 class ApiConfig(AppConfig):
     name = 'api'
     verbose_name = "ApiConfig"
     app = Celery('tasks', broker=settings.BROKER_URL)
+    app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
 
     @classmethod
-    @app.task
-    def send_slack_webhook(cls):
-        if settings.ENVIRONMENT == "development" and os.environ.get("CONTAINER_TYPE") == "portal_api":
-            return True
-        if os.environ.get("CONTAINER_TYPE") == "portal_admin":
-            data = {
-                "text": "PORTAL ADMIN " + settings.ALLOWED_HOSTS[0] + " is ready."
-            }
-        else:
-            data = {
-                "text": "PORTAL API " + settings.API_SETTINGS["WEB_URL"] + " is ready."
-            }
-        r = requests.post(
-            "https://hooks.slack.com/services/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",
-            data=json.dumps(data),
-            headers={
-                "Content-Type": "application/json"
-            }
-        )
-
-    @classmethod
-    def init_cache(cls, user):
+    def init_cache(cls, user_model):
         c = DefaultCache()
-        users = user.objects.all()
+        users = user_model.objects.all()
         for u in users:
             key = "user_" + str(u.pk)
             c.set(key, u, 0)
 
     def ready(self):
-        user = self.get_model('User')
-        self.send_slack_webhook()
-        self.init_cache(user)
+        from .tasks import released_indicators
+        self.init_cache(self.get_model('User'))
+        send_slack_webhook.delay()
+        released_indicators.delay()
+
+
+@ApiConfig.app.task
+def send_slack_webhook():
+    if settings.ENVIRONMENT == "development" and os.environ.get("CONTAINER_TYPE") == "portal_api":
+        return True
+    if os.environ.get("CONTAINER_TYPE") == "portal_admin":
+        data = {
+            "text": "PORTAL ADMIN " + settings.ALLOWED_HOSTS[0] + " is ready."
+        }
+    else:
+        data = {
+            "text": "PORTAL API " + settings.API_SETTINGS["WEB_URL"] + " is ready."
+        }
+    r = requests.post(
+        "https://hooks.slack.com/services/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",
+        data=json.dumps(data),
+        headers={
+            "Content-Type": "application/json"
+        }
+    )
