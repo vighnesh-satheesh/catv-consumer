1bcba0af654341ebd533795dfe2af1d6e81579ef
Adeel Siddiqui <34234631+rmiaas@users.noreply.github.com>
2020-02-25 16:50:57 +0800

    Add support for CATV path filtering. User can now send two addresses, specify a max depth to look for, and find whether a path exists between `address_from` and `address_to`.

--- api/constants.py
+++ api/constants.py
@@ -142,7 +142,13 @@ class Constants:
                                              "group by status) y on x.status = y.status;",
         "INSERT_SWAP_HISTORY_QUERY": "INSERT INTO api_exchange_token(user_id,sp_amount,status,req_time,upp) VALUES(%s,%s,%s,%s,%s);",
         "UPDATE_USER_POINTS_QUERY": "UPDATE api_user set points = (points-'{0}') where uid='{1}'",
-        "UPDATE_USER_POINTS_QUERY_REJECT": "UPDATE api_user set points = (points+'{0}') where uid='{1}'"
+        "UPDATE_USER_POINTS_QUERY_REJECT": "UPDATE api_user set points = (points+'{0}') where uid='{1}'",
+        "INSERT_USER_CATV_PATH_SEARCH": "INSERT INTO api_catv_path_history(user_id,address_from,address_to,depth, "
+                                        "from_date,to_date,logged_time,token_type,min_tx_amount, "
+                                        "limit_address_tx_count) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);",
+        "SELECT_USER_CATV_PATH": "select 0 as id, address_from, address_to, null as token_address, depth, from_date, "
+                                 "to_date from vw_catv_path_history where row_num = 1 and user_id={0} "
+                                 "and token_type='{1}' limit 10;",
     }
     CACHE_KEY = {
         "LEFT_PANEL_VALUES": "left_panel_values",
