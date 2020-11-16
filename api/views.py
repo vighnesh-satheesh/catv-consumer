bdb6fbd5c7f40173e3d26a1921f17481edc0a7ac
Adeel Siddiqui <34234631+rmiaas@users.noreply.github.com>
2020-11-16 14:40:21 +0800

    Fix cached response logic

--- api/views.py
+++ api/views.py
@@ -3305,8 +3305,6 @@ class SecurityTagView(generics.ListCreateAPIView):
         c = DefaultCache()
         cached_response = c.get_view_cache(request)
         if cached_response:
-            for item in cached_response["data"]["items"]:
-                item["label"] = _(item["tag"])
             return APIResponse(cached_response)
         queryset = self.model.objects.all()
         serializer = SecurityTagSerializer(queryset, many=True)
