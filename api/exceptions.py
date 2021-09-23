fe427af404e25c6ed8be30314b9b99cf7123185d
Kaustav98 <31511879+Kaustav98@users.noreply.github.com>
2021-09-23 16:28:19 +0530

    Update exceptions.py

--- api/exceptions.py
+++ api/exceptions.py
@@ -7,8 +7,6 @@ from rest_framework import exceptions
 #####
 # 400
 #####
-
-
 class ValidationError(exceptions.APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = _('Invalid input.')
@@ -18,8 +16,6 @@ class ValidationError(exceptions.APIException):
 #####
 # 401
 #####
-
-
 class AuthenticationCheckError(exceptions.AuthenticationFailed):
     default_detail = _('user not exist or password wrong.')
 
@@ -27,36 +23,12 @@ class AuthenticationCheckError(exceptions.AuthenticationFailed):
 #####
 # 404
 #####
-
-
-class UserNotFound(exceptions.NotFound):
-    default_detail = _('user not found')
-
 class FileNotFound(exceptions.NotFound):
     default_detail = _('file not found')
-
-class CATVReportNotFound(exceptions.NotFound):
-    default_detail = _('CATV Report not found')
-
-
-#####
-# 403
-#####
-
-
-class NotAllowedError(exceptions.PermissionDenied):
-    default_detail = _('no permission.')
-
 
 #####
 # 409
 #####
-
-
 class DataIntegrityError(exceptions.APIException):
     status_code = status.HTTP_409_CONFLICT
     default_detail = _('data conflict')
-
-class ServerError(exceptions.APIException):
-    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-    default_detail = _('Something went wrong')
