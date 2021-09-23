8196dacefb7061ffbca14ad361f62f71d6870444
Kaustav98 <31511879+Kaustav98@users.noreply.github.com>
2021-09-23 15:44:35 +0530

    removed router and views

--- api/router.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from rest_framework import routers
-from . import views
-
-router = routers.SimpleRouter()
-
-urlpatterns = [
-    url(r'^healthcheck/?$', views.HealthCheckView.as_view(), name='healthcheck'),
-]
