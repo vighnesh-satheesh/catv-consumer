c4abb5dac5d218815d3398e6c53ad5f00d30240b
Kaustav98 <31511879+Kaustav98@users.noreply.github.com>
2021-10-11 17:10:26 +0530

    Updated models.py and ran makemigrations

--- api/models.py
+++ api/models.py
@@ -152,8 +152,6 @@ class IndicatorExtraAnnotation(models.Model):
 class CatvResult(models.Model):
     request = models.ForeignKey(CatvRequestStatus, null=False,
                                 blank=False, on_delete=models.CASCADE, related_name='request')
-    # result_file = models.ForeignKey(
-    #     AttachedFile, null=True, blank=True, on_delete=models.CASCADE, related_name='result_file')
     result_file_id = models.IntegerField(null=True)
 
     class Meta:
