5f3c429d84cc8a28ed609fce13048b99d10eb1fe
Kaustav98 <31511879+Kaustav98@users.noreply.github.com>
2021-10-13 14:46:43 +0530

    Update development.py

--- portal_api/settings/development.py
+++ portal_api/settings/development.py
@@ -5,8 +5,10 @@ DEBUG = True
 
 
 ALLOWED_HOSTS += ["172.21.20.%s" % s for s in range(2, 255)]
+ALLOWED_HOSTS += ["10.12.49.%s" % s for s in range(2, 255)]
+ALLOWED_HOSTS += ["10.12.50.%s" % s for s in range(2, 255)]
 ALLOWED_HOSTS += [
-    "localhost", "test.sentinelportal.com"
+    "localhost", "test.sentinelportal.com", "https://stgcatv-consumer-service.api.sentinelprotocol.io"
 ]
 
 ALLOWED_HOSTS += env.list('ALLOWED_HOSTS', default=['*', ])
