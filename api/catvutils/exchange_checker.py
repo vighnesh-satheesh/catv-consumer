46e843c302fc2a87e2293cbdd6a14a4a309e295c
Kaustav98 <31511879+Kaustav98@users.noreply.github.com>
2021-11-17 13:47:18 +0530

    Update exchange_checker.py

--- api/catvutils/exchange_checker.py
+++ api/catvutils/exchange_checker.py
@@ -25,17 +25,41 @@ def src_exchanges(graph_data):
 
 def dist_exchanges(graph_data):
     dist_exchange_nodes = [node for node in graph_data['node_list'] if node['id']>=0 and node['group'] == 'Exchange & DEX']
-    nodes_after_exchange = [edge['to'] for edge in graph_data['edge_list'] if edge['from'] == dist_exchange_nodes[0]['id']]
-    edge_ids = []
     node_ids = []
+
     for node in dist_exchange_nodes:
-        temp = [edge for edge in graph_data['edge_list'] if edge['from'] == node['id']]
-        node_ids = node_ids + temp['to']
-        edge_ids = edge_ids + temp['id']
-    print("new edge list", node_ids, edge_ids)
+        node_ids += [edge['to'] for edge in graph_data['edge_list'] if edge['from'] == node['id']]
+
+
+    nodes_to_be_removed, edges_to_be_removed, node_enum_to_be_removed = remove_graph_data(graph_data, node_ids)
+    graph_data['node_list'] = [node for node in graph_data['node_list'] if node['id'] not in nodes_to_be_removed]
+    graph_data['edge_list'] = [edge for edge in graph_data['edge_list'] if edge['id'] not in edges_to_be_removed]
+    graph_data['node_enum'] = [node_enum for node_enum in graph_data['node_enum'] if node_enum['address'] not in node_enum_to_be_removed]
+    
+    print("new node list", node_ids)
     print("dist x-nodes ", dist_exchange_nodes)
-    print("nodes after x-node ", nodes_after_exchange)
     return graph_data
 
-def process_nodes_recursively():
-    pass
+def remove_graph_data(graph_data, initial_node_ids):
+    edge_list = graph_data['edge_list']
+    node_list = graph_data['node_list']
+    node_enum_to_be_removed = []
+    edges_to_be_removed = []
+    node_ids = initial_node_ids
+    nodes_to_be_removed = initial_node_ids
+    print("length of node list", len(node_ids))
+
+    for node_id in node_ids:
+        print("first line of loop", node_id)
+        node_ids += [edge['to'] for edge in edge_list if edge['from'] == node_id]
+        edges_to_be_removed += [edge['id'] for edge in edge_list if edge['from'] == node_id]
+        node_enum_to_be_removed += [node['address'] for node in node_list if node['id'] == node_id]
+        nodes_to_be_removed += node_ids
+        node_ids.remove(node_id)
+        print("last line of loop", node_ids)
+    
+    print("node list after node removal", nodes_to_be_removed)
+    return nodes_to_be_removed, edges_to_be_removed, node_enum_to_be_removed
+
+# [1,2,3,4,5]
+# [2,3,4,5,6,7]
