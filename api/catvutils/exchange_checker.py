9aea1908f0a473abe13e8120414e31d4bf2ea381
Kaustav98 <31511879+Kaustav98@users.noreply.github.com>
2021-11-19 17:42:36 +0530

    Update exchange_checker.py

--- api/catvutils/exchange_checker.py
+++ api/catvutils/exchange_checker.py
@@ -1,5 +1,4 @@
 def stop_transfers_at_exchange(graph_data, dist_analysis, src_analysis):
-    
     if len(dist_analysis['exchange'])==0 and len(src_analysis['exchange'])==0:
         print("No exchanges found")
         return graph_data
@@ -24,42 +23,69 @@ def src_exchanges(graph_data):
     return graph_data
 
 def dist_exchanges(graph_data):
-    dist_exchange_nodes = [node for node in graph_data['node_list'] if node['id']>=0 and node['group'] == 'Exchange & DEX']
-    node_ids = []
+    # Getting the initial list of exchange nodes
+    dist_exchange_nodes = [node['id'] for node in graph_data['node_list'] if node['id']>=0 and node['group'] == 'Exchange & DEX']
+    print("dist x-nodes", dist_exchange_nodes)
 
-    for node in dist_exchange_nodes:
-        node_ids += [edge['to'] for edge in graph_data['edge_list'] if edge['from'] == node['id']]
+    # Data after calling the remove_graph_data method
+    nodes_to_be_removed, node_enum_to_be_removed = remove_graph_data(graph_data, dist_exchange_nodes)
 
+    # Processing node_list
+    graph_data['node_list'] = [
+        node for node in graph_data['node_list'] 
+            if node['id'] not in nodes_to_be_removed
+    ]
+    # Processing edge_list
+    graph_data['edge_list'] = [
+        edge for edge in graph_data['edge_list'] 
+            if edge['from'] not in nodes_to_be_removed 
+            if edge['to'] not in nodes_to_be_removed
+    ]
+    # Processing node_enum dict
+    for node_address in node_enum_to_be_removed:
+        graph_data['node_enum'].pop(node_address, None)
 
-    nodes_to_be_removed, edges_to_be_removed, node_enum_to_be_removed = remove_graph_data(graph_data, node_ids)
-    graph_data['node_list'] = [node for node in graph_data['node_list'] if node['id'] not in nodes_to_be_removed]
-    graph_data['edge_list'] = [edge for edge in graph_data['edge_list'] if edge['id'] not in edges_to_be_removed]
-    graph_data['node_enum'] = [node_enum for node_enum in graph_data['node_enum'] if node_enum['address'] not in node_enum_to_be_removed]
-    
-    print("new node list", node_ids)
-    print("dist x-nodes ", dist_exchange_nodes)
+    print("nodes to be removed", nodes_to_be_removed)
     return graph_data
 
-def remove_graph_data(graph_data, initial_node_ids):
+def remove_graph_data(graph_data, exchange_node_ids):
     edge_list = graph_data['edge_list']
     node_list = graph_data['node_list']
     node_enum_to_be_removed = []
-    edges_to_be_removed = []
-    node_ids = initial_node_ids
-    nodes_to_be_removed = initial_node_ids
-    print("length of node list", len(node_ids))
+    exchange_node_ids_iter = exchange_node_ids
+    nodes_to_be_removed = []
+    print("length of node list", len(exchange_node_ids_iter))
 
-    for node_id in node_ids:
-        print("first line of loop", node_id)
-        node_ids += [edge['to'] for edge in edge_list if edge['from'] == node_id]
-        edges_to_be_removed += [edge['id'] for edge in edge_list if edge['from'] == node_id]
-        node_enum_to_be_removed += [node['address'] for node in node_list if node['id'] == node_id]
-        nodes_to_be_removed += node_ids
-        node_ids.remove(node_id)
-        print("last line of loop", node_ids)
+    # for node_id in exchange_node_ids_iter:
+    #     nodes_after_exchange = []
+    #     print(exchange_node_ids_iter.index(node_id),"-exchange ", node_id)
+    #     # exchange_node_ids_iter += [edge['to'] for edge in edge_list if edge['from'] == node_id]
+    #     for edge in edge_list:
+    #         if edge['from'] == node_id:
+    #             nodes_after_exchange.append(edge['to'])
+    #     print("Nodes after exchange", nodes_after_exchange)
+
+    #     node_enum_to_be_removed += [node['address'] for node in node_list if node['id'] == node_id]
+    #     nodes_to_be_removed += nodes_after_exchange
+    #     exchange_node_ids_iter += nodes_after_exchange
+        # break
     
-    print("node list after node removal", nodes_to_be_removed)
-    return nodes_to_be_removed, edges_to_be_removed, node_enum_to_be_removed
+    
+    nodes_to_be_removed = find_subsequent_nodes(node_list, edge_list, exchange_node_ids)
+    print("nodes to be removed from inside method", nodes_to_be_removed)
+    print("exchange_node_ids_iter", exchange_node_ids_iter)
+    print("node_enum_to_be_removed", node_enum_to_be_removed)
+    return nodes_to_be_removed, node_enum_to_be_removed
 
-# [1,2,3,4,5]
-# [2,3,4,5,6,7]
+def find_subsequent_nodes(node_list, edge_list, exchange_node_ids):
+    exchange_node_ids_iter = exchange_node_ids
+    for node_id in exchange_node_ids_iter:
+        nodes_after_exchange = []
+        print(exchange_node_ids_iter.index(node_id),"-exchange ", node_id)
+        # exchange_node_ids_iter += [edge['to'] for edge in edge_list if edge['from'] == node_id]
+        for edge in edge_list:
+            if edge['from'] == node_id:
+                nodes_after_exchange.append(edge['to'])
+        print("Nodes after exchange", nodes_after_exchange)
+    
+    return nodes_after_exchange
